8a1444e51f879f3ade719bb751b58bebdc3c913b
vukasinb7 <51921035+vukasinb7@users.noreply.github.com>
2023-06-21 19:58:18 +0200

    Imported file with all urls

--- threecloud/lib/models/resource.dart
+++ threecloud/lib/models/resource.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/io_client.dart';
-import 'package:threecloud/url.dart';
+import 'package:threecloud/urls.dart';
 
 class Resource {
   static Future<String> upload(String name, String image,List<Map<String,String>> tags) async
--- threecloud/lib/models/user.dart
+++ threecloud/lib/models/user.dart
@@ -2,7 +2,7 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:http/io_client.dart';
-import 'package:threecloud/url.dart';
+import 'package:threecloud/urls.dart';
 
 
 
