d1981bd24cc3d3c5d0571d9b51585c29bb0ccfe1
mrmijan <mijanovic.boki.bojan@gmail.com>
2023-06-26 19:28:13 +0200

    Resolving conflicts

--- back/utility/dynamo_resources.py
+++ back/utility/dynamo_resources.py
@@ -3,11 +3,7 @@ from datetime import datetime
 
 import boto3
 
-<<<<<<< HEAD
 from utility.dynamo_directory import find_directory_by_path, insert_directory_in_dynamo
-=======
-from utility.dynamo_directory import find_directory_by_path
->>>>>>> development
 
 table_name_resources = os.environ['RESOURCES_TABLE_NAME']
 
@@ -40,7 +36,7 @@ def delete_resource_from_dynamo(path):
 
 
 def update_path_for_resource_in_dynamo(path, new_path):
-    item = find_file_by_path(path)[0]
+    item = find_resource_by_path(path)[0]
 
     if new_path[-1] == "/":
         new_path = new_path[:-1]
@@ -49,7 +45,7 @@ def update_path_for_resource_in_dynamo(path, new_path):
     item['timeUpdated'] = str(datetime.now())
 
     delete_resource_from_dynamo(path)
-    insert_file_in_dynamo(item)
+    insert_resource_in_dynamo(item)
 
     new_directory = find_directory_by_path(new_path)[0]
     new_directory['items'] += [item['path']]
@@ -58,6 +54,8 @@ def update_path_for_resource_in_dynamo(path, new_path):
     old_directory = find_directory_by_path("/".join(path.split("/")[:-1]))[0]
     old_directory['items'].remove(path)
     insert_directory_in_dynamo(old_directory)
+
+
 def check_parent(user, path):
     if "/" not in path:
         return False
